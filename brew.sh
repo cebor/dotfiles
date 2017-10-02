c5648499f4e69d371ea553401e2fc847f8a32c31
Felix Itzenplitz <cebor@users.noreply.github.com>
2017-10-02 20:23:34 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -16,7 +16,7 @@ cask_install () {
 # install homebrew
 if [ ! -x "$(which brew)" ]; then
   echo "Installing Homebrew."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew prune
