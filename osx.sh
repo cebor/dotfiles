71b42b650b680379ea85758d09d96c1b1530cc24
Felix Itzenplitz <cebor@users.noreply.github.com>
2017-10-27 11:28:28 +0200

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -2,6 +2,7 @@
 cd "$(dirname "$0")"
 
 read -p "Hostname: "
-sudo scutil --set ComputerName "$REPLY"
-sudo scutil --set LocalHostName "$REPLY"
-sudo scutil --set HostName "$REPLY"
+sudo scutil --set ComputerName "$REPLY" && \
+sudo scutil --set HostName "$REPLY" && \
+sudo scutil --set LocalHostName "$REPLY" && \
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$REPLY"
