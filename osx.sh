e146517e6c2fd123a0e45f1ada2293b64b248e85
Felix Itzenplitz <felix@stkn.org>
2017-11-06 16:18:47 +0100

    add some write cmds

--- osx.sh
+++ osx.sh
@@ -6,3 +6,16 @@ sudo scutil --set ComputerName "$REPLY" && \
 sudo scutil --set HostName "$REPLY" && \
 sudo scutil --set LocalHostName "$REPLY" && \
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$REPLY"
+
+# finder
+defaults write com.apple.finder NewWindowTarget -string "PfHm"
+defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/"
+defaults write com.apple.finder ShowRecentTags -bool false
+defaults write -g AppleShowAllExtensions -bool true
+
+# dock
+defaults write com.apple.dock minimize-to-application -bool true
+defaults write com.apple.dock autohide -bool true
+
+killall Finder
+killall Dock
