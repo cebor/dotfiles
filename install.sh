2ba91a9a80748ea341e61104b05fc2f04fbdcde0
Felix Itzenplitz <felix@stkn.org>
2018-01-11 02:11:21 +0100

    move files

--- install.sh
+++ install.sh
@@ -55,7 +55,7 @@ pip3 install --upgrade ipython
 # angular-cli
 yarn global add @angular/cli
 ng set --global packageManager=yarn
-ng completion --zsh >> "$HOME"/.dotfiles/completions
+ng completion --zsh >> "$HOME"/.completions
 
 # rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys \
