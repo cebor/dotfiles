3cc640df2fed2ea2cc7f4d0fe01fe10cd99c82c3
Felix Itzenplitz <felix@stkn.org>
2020-01-14 11:58:57 +0100

    move brews and caks to .txt files

--- brew.sh
+++ brew.sh
@@ -12,27 +12,22 @@ cask_install () {
   brew cask install "$1"
 }
 
-# install homebrew
 if [ ! -x "$(which brew)" ]; then
   echo "Installing Homebrew."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew doctor
 brew update
 brew upgrade
 
 brew tap mongodb/brew
 
-# install brews
-for brew in {coreutils,findutils,zsh,wget,rsync,git,python,ipython,ctags,node,vim,antigen,yarn,httpie,gpg,wrk,tree,watch,nmap,mtr,kubernetes-cli,kubernetes-helm,mongodb-community}; do
+while read brew; do
   brew_install "$brew"
-done
+done <brews.txt
 
-brew cask doctor
-
-for cask in {google-chrome,iterm2,visual-studio-code,docker,github,vlc,spotify,skype,whatsapp,teamspeak-client,wireshark}; do
+while read cask; do
   cask_install "$cask"
-done
+done <casks.txt
 
 brew cleanup
