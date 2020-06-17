b07dce9c8e0d7189a135e273a88ef819e788bb4e
Felix Itzenplitz <cebor@users.noreply.github.com>
2020-06-17 22:50:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,10 +14,6 @@ xcode-select --install
 ./brew.sh
 
 # setup zsh
-BREWED_ZSH="/usr/local/bin/zsh"
-if ! grep -Fxq "$BREWED_ZSH" /etc/shells; then
-  echo "$BREWED_ZSH" | sudo tee -a /etc/shells
-fi
 echo "Changing shell to ZSH."
 sudo chsh -s "$BREWED_ZSH" "$USER"
 
